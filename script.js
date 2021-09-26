b0f8b6363310d891f197a86027332624c2f69609
pj2083 <88223457+pj2083@users.noreply.github.com>
2021-09-26 13:37:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+let hr=0;
+let min=0;
+let sec=0;
+let count=0;
+let timer=false;
+function start(){
+timer=true;
+stopwatch();
+}
+function stop(){
+timer=false;
+}
+function reset(){
+timer=false;
+hr=0;
+min=0;
+sec=0;
+document.getElementById("hr").innerHTML="00";
+document.getElementById("min").innerHTML="00";
+document.getElementById("sec").innerHTML="00";
+document.getElementById("count").innerHTML="00";
+}
+function stopwatch(){
+    if(timer==true){
+     count=count+1;
+     if(count==100){
+        sec=sec+1;
+        count=0;
+        if(sec==60){
+            min=min+1;
+            sec=0;
+        }
+        if(min==60){
+            hr=hr+1;
+            min=0;
+        }
+    }
+    let hrStr=hr;
+    let minStr=min;
+    let secStr=sec;
+    let countStr=count;
+  if(hr<10){
+      hrStr="0"+hrStr;
+  }
+  if(min<10){
+    minStr="0"+minStr;
+}
+if(sec<10){
+secStr="0"+secStr;
+}
+if(count<10){
+    countStr="0"+countStr;
+}
+
+    document.getElementById("hr").innerHTML=hrStr;
+    document.getElementById("min").innerHTML=minStr;
+    document.getElementById("sec").innerHTML=secStr;
+     document.getElementById("count").innerHTML=countStr;
+        setTimeout("stopwatch()",10);
+    }
+    
+}
